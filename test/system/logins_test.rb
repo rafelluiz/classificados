6d76fd87c351f7c3802fedfb0df36807acf08e5c
Rafael Luiz de Araújo Oliveira <rafelluiz@gmail.com>
2021-01-02 15:08:18 -0300

    Tela Login Criada

--- test/system/logins_test.rb
+++ test/system/logins_test.rb
@@ -10,14 +10,14 @@ class LoginsTest < ApplicationSystemTestCase
     user = FactoryBot.create(:user, password: "password", password_confirmation: "password")
 
     visit root_path
-    click_on "Entrar"
+    click_link "Entrar"
     
     fill_in "E-mail", with: user.email
     fill_in "Senha", with: "password"
 
-    click_on "Entrar"
+    click_button "Entrar"
 
-    assert_text "Bem vindo rafael"
+    assert_text "Bem vindo rafael!"
 
   end
 end

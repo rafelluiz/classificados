375775bebf190bc21865bca27ebac11aa4529919
Rafael Luiz de Araújo Oliveira <rafelluiz@gmail.com>
2021-01-02 15:46:10 -0300

    Testes automatizado da tela de login criados

--- test/system/logins_test.rb
+++ test/system/logins_test.rb
@@ -1,23 +1,32 @@
 require "application_system_test_case"
 
 class LoginsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit logins_url
-  #
-  #   assert_selector "h1", text: "Login"
-  # end
+  def setup
+    @user = FactoryBot.create(:user, password: "password", password_confirmation: "password")
+  end
   test "login com credenciais validas" do
-    user = FactoryBot.create(:user, password: "password", password_confirmation: "password")
-
+    
     visit root_path
     click_link "Entrar"
     
-    fill_in "E-mail", with: user.email
+    fill_in "E-mail", with: @user.email
     fill_in "Senha", with: "password"
 
     click_button "Entrar"
 
     assert_text "Bem vindo rafael!"
+  end
 
+  test "login com credenciais invalidas" do
+    visit root_path
+    click_link "Entrar"
+    
+    fill_in "E-mail", with: @user.email
+    fill_in "Senha", with: "invalidpassword"
+
+    click_button "Entrar"
+
+    assert_equal new_session_path, current_path
+    assert_text "E-mail ou senha inválidos."
   end
 end
